62d4f49fb07da4286ef29cdd644ae17a3b98bcc7
Kacper <kacper.m.wojtowicz@gmail.com>
2022-06-22 17:57:12 +0200

    prepare for sass

--- js/projects.js
+++
@@ -1,9 +0,0 @@
-const projects = {
-  test1: {
-    name: 'test',
-    id: 1,
-    github: 'https://github.com/thatguyKacper/test',
-    githubPages: 'https://thatguykacper.github.io/test/',
-    img: '/img/Zrzut ekranu z 2022-02-03 11-43-14.png',
-  },
-};
